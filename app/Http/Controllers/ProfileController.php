061d023f4c66b2b7b157d1ef0627b8bf304531fa
Jolodong3 <grahitanicholas@gmail.com>
2025-11-04 11:51:47 +0700

    Add index method to ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,1 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    public function index()
+    {
+        $user = Auth::guard('pembeli')->user(); // ambil data pembeli yang sedang login
+        return view('profile', compact('user'));
+    }
+}
 
